983abfb0f83accd7cfc824ab95a2733a203d50a7
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-19 23:18:25 +0200

    change branch

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-user_name = input("Enter your name: ")
-print("Hello , glad to see you! ")
+user_name = input("Enter your name : ")
+print("Glad to see you " + user_name)
 
-user_email = input("Enter your user_email: ")
-print( user_email )
+user_email = input("Eneter your email : ")
+print(user_email)
 
-user_age = input("Enter your year of birth: ")
-print int( user_age )
+user_age = input("Enter your year of birth : ")
+print int(user_age)
