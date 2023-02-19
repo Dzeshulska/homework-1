d786799ce62ceae7fe89f55de9e4effb35c749f9
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-19 22:36:27 +0200

    added work

---
+++ app.py
@@ -0,0 +1,8 @@
+user_name = input("Enter your name: ")
+print("Hello , glad to see you! ")
+
+user_email = input("Enter your user_email: ")
+print( user_email )
+
+user_age = input("Enter your year of birth: ")
+print int( user_age )
