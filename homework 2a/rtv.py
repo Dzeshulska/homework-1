5221a316df9ef498213b64e452a3e18d93e0a935
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-22 21:42:18 +0200

    added part homework

---
+++ homework 2a/rtv.py
@@ -0,0 +1,14 @@
+user_name = input("enter your name")
+print(user_name)
+user_name = "jOHn"
+print(user_name.lower().capitalize())
+sur_name = input("Enter your sur_name")
+sur_name = "DOE"
+a = sur_name.split( )
+print(a)
+
+
+
+
+
+
