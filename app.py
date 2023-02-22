db474e258e695cd80a2a54237c5816df9776e5c9
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-22 20:06:33 +0200

    added change into howework 1

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-user_name = input("Enter your name : ")
-print("Glad to see you " + user_name)
-
-user_email = input("Eneter your email : ")
+user_name = input("Enter your name")
+print(f"Hello, glad to see you {user_name}")
+user_email = input("Enter your user_email :")
 print(user_email)
-
-user_age = input("Enter your year of birth : ")
-print int(user_age)
+user_age = input("Enter your year of birth :")
+print(user_age)
