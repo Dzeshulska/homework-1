c6fe07495b0f85188ef2b9bf3d1878debeee87a0
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-23 23:57:01 +0200

    added performance

--- homework 2a/rtv.py
+++ homework 2a/rtv.py
@@ -1,20 +1,31 @@
 user_name = input("enter your name")
-print(user_name .lower() .capitalize())
-
+print(user_name.lower().capitalize())
 
 sur_name = input("Enter your sur_name")
-L = " " .join(sur_name)
+L = " " .join(sur_name) 
 print(L)
 
-
 user_age = input("Enter how old are you?")
-print(user_age)
-if int(user_age) == 13:
-    print(user_age)
+if int(user_age) > 13:
+    print("Non")
+elif int(user_age) < 13:
+    print("Not that")
+else:
+    print("13")
 
+if int(user_age) > 15:
+    print("Non")
+elif int(user_age) < 15:
+    print("Not that")
+else:
+    print("15")
  
- 
-
+ if int(user_age) > 23:
+    print("Non")
+elif int(user_age) < 23:
+    print("Not that")
+else:
+    print("23")
 
 expression = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium nihil
 laborum rerum ipsum iure dicta officiis, necessitatibus non vel excepturi minima
@@ -23,13 +34,11 @@ eius sapiente aperiam vero? Eum ducimus consequuntur, accusamus tempore corporis
 numquam animi hic ad voluptatem saepe mollitia voluptate explicabo soluta earum!
 Consequatur porro eos minus facilis mollitia alias nesciunt. Molestias explicabo
 alias ea praesentium placeat ex ad maxime ipsam non velit architecto, iure
-laborum reiciendis pariatur dolorem amet nulla dolor quos a esse atque vel? Fuga
+laborum reiciendis pariatur do lorem amet nulla dolor quos a esse atque vel? Fuga
 quidem perspiciatis velit iure excepturi. Delectus velit amet distinctio error
 temporibus at voluptas suscipit laboriosam rerum ea, quibusdam nesciunt maiores
 quas necessitatibus fugit veniam, molestiae inventore dignissimos voluptatum
 libero enim natus! Cum, corrupti ad."""
-
- 
 
 print(len(expression))
   
@@ -38,10 +47,18 @@ print("ad" in expression)
 print(expression.find("ad"))
 
 print(expression[39:41])
+
+
+print(len(expression))
  
 print(expression.find("Lorem"))
 
 print(expression.replace("Lorem", "hey you"))
+
+
+
+
+
 
 
 
